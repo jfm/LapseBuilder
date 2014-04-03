26bbfac16726af99b8cf514b060e3498b0f74bc1
Jesper Fussing Mørk <jfm@moerks.dk>
2014-04-03 10:10:21 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from distutils.core import setup
 import sys
 
-sys.path.append('googlemaps')
+sys.path.append('lapsebuilder')
 import googlemaps
 
 
-setup(name='googlemaps',
+setup(name='lapsebuilder',
       version='1.0',
       author='Jesper Fussing Mørk',
       author_email='jfm@moerks.dk',
       url='https://github.com/jfm/LapseBuilder/',
       download_url='',
       description='Easy to use Python Application for creating TimeLapse videos using ImageMagick and FFMpeg',
-      long_description=googlemaps.GoogleMaps.__doc__,
-      packages=['im', 'video']
+      long_description=LapseBuilder.__doc__,
+      package_dir=['lapsebuilder']
+      packages=['system', 'video', 'image']
      )
