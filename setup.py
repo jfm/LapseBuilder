1894e3341079207464abdb264d91838836b86eb7
Jesper Fussing Moerk <jfm@moerks.dk>
2014-04-03 20:37:19 +0200

    Removed faulty Long Description

--- setup.py
+++ setup.py
@@ -10,6 +10,5 @@ setup(name='lapsebuilder',
       url='https://github.com/jfm/LapseBuilder/',
       download_url='',
       description='Easy to use Python Application for creating TimeLapse videos using ImageMagick and FFMpeg',
-      long_description='Hello World',
       packages=['lapsebuilder', 'lapsebuilder.system', 'lapsebuilder.video', 'lapsebuilder.image'],
       py_modules=['LapseBuilder'])
